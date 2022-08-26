a2b915f8515383671b4ae9f56d3256bff1de0b03
ShalihRizal <rizalpangandaran9@gmail.com>
2022-08-26 08:38:51 +0800

    add event

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,53 +1,69 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using Paintastic.ScoreManager;
 
-public class Timer : MonoBehaviour
+namespace Paintastic.Timer
 {
-    [SerializeField]
-    float initialTime = 15f;
-
-    [SerializeField]
-    private TextMeshProUGUI timerText;
-
-    [SerializeField]
-    ScoreManager scoreManager;
-
-    private void Update()
+    public class Timer : MonoBehaviour
     {
-        if (initialTime > 0)
+
+        public Action OnTimeToSpawn;
+
+        [SerializeField]
+        float initialTime = 15f;
+
+        [SerializeField]
+        float timeToSpawn = 8f;
+
+        [SerializeField]
+        private TextMeshProUGUI timerText;
+
+        [SerializeField]
+        ScoreManager.ScoreManager scoreManager;
+
+        private void Update()
         {
-            initialTime -= Time.deltaTime;
+            if (initialTime > 0)
+            {
+                initialTime -= Time.deltaTime;
+                timeToSpawn -= Time.deltaTime;
+
+                if (timeToSpawn <= 0)
+                {
+                    OnTimeToSpawn?.Invoke();
+                    timeToSpawn = 8;
+                }
+            }
+            else
+            {
+                initialTime = 0;
+                scoreManager.OnTimesUp();
+                Time.timeScale = 0;
+
+            }
+
+            DisplayTime(initialTime);
         }
-        else
+
+        private void Awake()
         {
-            initialTime = 0;
-            scoreManager.OnTimesUp();
-            Time.timeScale = 0;
-            
+            Time.timeScale = 1;
         }
 
-        DisplayTime(initialTime);
-    }
-
-    private void Awake()
-    {
-        Time.timeScale = 1;
-    }
-
-    void DisplayTime(float amountToDisplay)
-    {
-        if (amountToDisplay < 0)
+        void DisplayTime(float amountToDisplay)
         {
-            amountToDisplay = 0;
+            if (amountToDisplay < 0)
+            {
+                amountToDisplay = 0;
+            }
+
+            float minutes = Mathf.FloorToInt(amountToDisplay / 60);
+            float seconds = Mathf.FloorToInt(amountToDisplay % 60);
+
+            timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
         }
-
-        float minutes = Mathf.FloorToInt(amountToDisplay / 60);
-        float seconds = Mathf.FloorToInt(amountToDisplay % 60);
-
-        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 }
